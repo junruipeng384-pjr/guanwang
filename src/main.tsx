d50020cc3b28ca4a02859cf079df089a1e5f7062
Pengjunrui <junruipeng384@gmail.com>
2026-06-25 17:02:17 +0800

    feat: switch from HashRouter to BrowserRouter, add 404.html for GitHub Pages SPA routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import { CartProvider } from "./hooks/useCart";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
